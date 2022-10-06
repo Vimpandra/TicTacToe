07bbeca65c2540d603042f7ae89992c990e92c59
Vimpandra <victorimperatriz@gmail.com>
2022-10-06 18:34:14 -0300

    Added proper functionality to the BasicAI and a check for win/draw

--- script.js
+++ script.js
@@ -28,6 +28,18 @@ const Board = (function() {
             (squares[2].textContent === squares[4].textContent && squares[4].textContent === squares[6].textContent && squares[2].textContent !== ``)
         ) {
             console.log(`Winner`);
+        } else if (
+            squares[0].textContent !== `` &&
+            squares[1].textContent !== `` &&
+            squares[2].textContent !== `` &&
+            squares[3].textContent !== `` &&
+            squares[4].textContent !== `` &&
+            squares[5].textContent !== `` &&
+            squares[6].textContent !== `` &&
+            squares[7].textContent !== `` &&
+            squares[8].textContent !== ``
+        ) {
+            console.log(`It's a draw`);
         }
     }
     return {
@@ -60,6 +72,7 @@ const gameControl = (function() {
                 player1.playSymbol(square);
                 Board.checkWin();
                 basicAI.randomPlay();
+                Board.checkWin();
             });
         });
     });
@@ -72,7 +85,23 @@ const basicAI = {
     symbol: `O`,
 
     randomPlay: function() {
-        let randomTarget = Math.floor(Math.random() * Board.squares.random);
-        console.log(randomTarget);
+        let randomTarget = Math.floor(Math.random() * Board.squares.length);
+        if(Board.squares[randomTarget].textContent == ``) {
+            Board.squares[randomTarget].textContent = this.symbol;
+        } else if (
+            Board.squares[0].textContent !== `` &&
+            Board.squares[1].textContent !== `` &&
+            Board.squares[2].textContent !== `` &&
+            Board.squares[3].textContent !== `` &&
+            Board.squares[4].textContent !== `` &&
+            Board.squares[5].textContent !== `` &&
+            Board.squares[6].textContent !== `` &&
+            Board.squares[7].textContent !== `` &&
+            Board.squares[8].textContent !== ``
+        ) {
+            return;
+        } else {
+            basicAI.randomPlay();
+        }
     },
 }
