591893b93595b2696984c7436d5a6596629644a3
Vimpandra <victorimperatriz@gmail.com>
2022-10-18 22:30:05 -0300

    Added a function that checks for the winner and draws the endScreen

--- README.md
+++ README.md
@@ -1,11 +1,2 @@
 # TicTacToe
 My TicTacToe project for The Odin Project's fullstack javascript developer course.
-
-To Do:
-    
-- Add option to input player name
-- find a way to save which player made the winning move
-- make the function that builds the end screen's messages
-- keep score between games
-- add the PvP mode
-- let player choose symbol: 'X' goes first / 'O' goes last
--- script.js
+++ script.js
@@ -22,20 +22,29 @@ const Board = (function() {
     
     function checkWin() {
         if (
-            (squares[0].textContent === squares[1].textContent && squares[1].textContent === squares[2].textContent && squares[0].textContent !== ``) ||
-            (squares[3].textContent === squares[4].textContent && squares[4].textContent === squares[5].textContent && squares[3].textContent !== ``) ||
-            (squares[6].textContent === squares[7].textContent && squares[7].textContent === squares[8].textContent && squares[6].textContent !== ``) ||
-    
-            (squares[0].textContent === squares[3].textContent && squares[3].textContent === squares[6].textContent && squares[0].textContent !== ``) ||
-            (squares[1].textContent === squares[4].textContent && squares[4].textContent === squares[7].textContent && squares[1].textContent !== ``) ||
-            (squares[2].textContent === squares[5].textContent && squares[5].textContent === squares[8].textContent && squares[2].textContent !== ``) ||
-    
-            (squares[0].textContent === squares[4].textContent && squares[4].textContent === squares[8].textContent && squares[0].textContent !== ``) ||
-            (squares[2].textContent === squares[4].textContent && squares[4].textContent === squares[6].textContent && squares[2].textContent !== ``)
+            (squares[0].textContent === `X` && squares[1].textContent === `X` && squares[2].textContent === `X`) ||
+            (squares[3].textContent === `X` && squares[4].textContent === `X` && squares[5].textContent === `X`) ||
+            (squares[6].textContent === `X` && squares[7].textContent === `X` && squares[8].textContent === `X`) ||
+            (squares[0].textContent === `X` && squares[3].textContent === `X` && squares[6].textContent === `X`) ||
+            (squares[1].textContent === `X` && squares[4].textContent === `X` && squares[7].textContent === `X`) ||
+            (squares[2].textContent === `X` && squares[5].textContent === `X` && squares[8].textContent === `X`) ||
+            (squares[0].textContent === `X` && squares[4].textContent === `X` && squares[8].textContent === `X`) ||
+            (squares[2].textContent === `X` && squares[4].textContent === `X` && squares[6].textContent === `X`)
         ) {
-            gameControl.gameScreen.classList.add(`hidden`);
-            gameControl.endScreen.classList.remove(`hidden`);
+            return `X`
         } else if (
+            (squares[0].textContent === `O` && squares[1].textContent === `O` && squares[2].textContent === `O`) ||
+            (squares[3].textContent === `O` && squares[4].textContent === `O` && squares[5].textContent === `O`) ||
+            (squares[6].textContent === `O` && squares[7].textContent === `O` && squares[8].textContent === `O`) ||
+            (squares[0].textContent === `O` && squares[3].textContent === `O` && squares[6].textContent === `O`) ||
+            (squares[1].textContent === `O` && squares[4].textContent === `O` && squares[7].textContent === `O`) ||
+            (squares[2].textContent === `O` && squares[5].textContent === `O` && squares[8].textContent === `O`) ||
+            (squares[0].textContent === `O` && squares[4].textContent === `O` && squares[8].textContent === `O`) ||
+            (squares[2].textContent === `O` && squares[4].textContent === `O` && squares[6].textContent === `O`)
+        ) {
+            return `O`;
+        }
+        else if (
             squares[0].textContent !== `` &&
             squares[1].textContent !== `` &&
             squares[2].textContent !== `` &&
@@ -46,7 +55,7 @@ const Board = (function() {
             squares[7].textContent !== `` &&
             squares[8].textContent !== ``
         ) {
-            console.log(`It's a draw`);
+            return `Draw`;
         }
     }
     return {
@@ -67,13 +76,18 @@ const gameControl = (function() {
 
     const pvpBtn = document.getElementById(`pvpBtn`);
     const pvaiBtn = document.getElementById(`pvaiBtn`);
+    
     const playAgainBtn = document.getElementById(`playAgainBtn`);
     const changeModeBtn = document.getElementById(`changeModeBtn`);
     
     const startPvaiBtn = document.getElementById(`startPvaiBtn`);
     const playerName = document.getElementById(`playerName`);
 
-    
+    let player1;
+    let player2;
+
+    let player1score = 0;
+    let player2score = 0;
 
     pvpBtn.addEventListener(`click`, () => {
         greetingScreen.classList.add(`hidden`);
@@ -89,19 +103,31 @@ const gameControl = (function() {
         pvaiInput.classList.add(`hidden`);
         gameScreen.classList.remove(`hidden`);
         
-        let player1;
         if (document.getElementById(`symbolX`).checked) {
             player1 = PlayerFactory(playerName.value, `X`);
+            player2 = basicAI;
             Board.squares.forEach(square => {
                 square.addEventListener(`click`, () => {
                     player1.playSymbol(square);
-                    Board.checkWin();
+                    if (Board.checkWin() === `X`) {
+                        player1score += 1;
+                        drawEndScreen(player1);
+                    } else if (Board.checkWin() === `Draw`) {
+                        drawEndScreen(`draw`)
+                    }
+
                     basicAI.randomPlay(`O`);
-                    Board.checkWin();
+                    if (Board.checkWin() === `O`) {
+                        player2score += 1;
+                        drawEndScreen(player2);
+                    } else if (Board.checkWin() === `Draw`) {
+                        drawEndScreen(`draw`)
+                    }
                 });
             });
         } else if (document.getElementById(`symbolO`).checked) {
             player1 = PlayerFactory(playerName.value, `O`);
+            player2 = basicAI;
             basicAI.randomPlay(`X`)
             Board.squares.forEach(square => {
                 square.addEventListener(`click`, () => {
@@ -120,8 +146,27 @@ const gameControl = (function() {
         gameScreen.classList.remove(`hidden`);
     });
 
+    function drawEndScreen(winner) {
+        const mainText = document.getElementById(`mainText`);
+        const gameScore = document.getElementById(`gameScore`);
+        const topText = document.getElementById(`topText`);
+
+        gameScreen.classList.add(`hidden`);
+        endScreen.classList.remove(`hidden`);
+
+        if (winner === `draw`) {
+            topText.textContent = `Oh no!`
+            mainText.textContent = `It's a draw, nobody wins`;
+            gameScore.innerHTML = `${player1.name} <strong>${player1score} X ${player2score}</strong> ${player2.name}`;
+        } else {   
+            topText.textContent = `Congratulations!`
+            mainText.textContent = `${winner.name} is the winner`;
+            gameScore.innerHTML = `${player1.name} <strong>${player1score} X ${player2score}</strong> ${player2.name}`;
+        }
+    }
+
     return {
-        greetingScreen, gameScreen, endScreen,
+        greetingScreen, gameScreen, endScreen, player1, player2,
     }
 })();
 
@@ -140,10 +185,7 @@ const basicAI = {
             Board.squares[6].textContent != `` &&
             Board.squares[7].textContent != `` &&
             Board.squares[8].textContent != ``
-        ) {
-            return
-        }
-
+        ) return
         let randomTarget;  
         do {
             randomTarget = Math.floor(Math.random() * Board.squares.length);
